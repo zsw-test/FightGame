18397295bcb5c994f2096b8fa0e0ffaf63ccd5fb
Shuang-zl <3130884348@qq.com>
2020-07-02 16:10:04 +0800

    202007021608

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    public Sprite[] Choices;
+    private SpriteRenderer render;
+    // Start is called before the first frame update
+    void Start()
+    {
+        render = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
